888c0124eea98e7c4e6aa975343b6151c498bcb8
Lucca Kaiser <luccakaiser@gmail.com>
2018-11-05 17:23:28 +0100

    add flat dynamic view

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+["papillard", "ssaunier", "tococorocko", "od91"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
